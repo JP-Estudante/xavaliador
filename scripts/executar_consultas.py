9aa79ae1be4ed0d0d1e4b475b8710337d7635e7c
João Pedro <joaopedroscheffler@gmail.com>
2026-05-01 12:21:24 -0300

    Remove expansao manual das consultas

--- scripts/executar_consultas.py
+++ scripts/executar_consultas.py
@@ -10,14 +10,6 @@ qp.set_database(db)
 # Sem stemming (configuração padrão)
 qp.set_default_op(xapian.Query.OP_OR)
 
-expansao_consulta = {
-    "10.2452/201-AH": "incêndio fogo bombeiros casa pessoas prédio predio desalojadas desalojada barracas barracos residência residencia edifício edificio curto-circuito guindais galifões camões lavra mouzinho",
-    "10.2452/204-AH": "avalanche avalancha avalanches mortos morte alpinistas neve soterrados soterrado nepal himalaias k2 islândia islandia montanhistas desaparecidos vítimas vitimas",
-    "10.2452/207-AH": "ferimentos fogo artifício artificio foguetes pirotecnia explosão acidente mortes feridos",
-    "10.2452/209-AH": "Miguel Induráin Indurain Tour France ciclista espanhol venceu quinta vez consecutiva",
-    "10.2452/210-AH": "nobel paz prêmio prémio dissidente chinês chines Wei Jingsheng Ximenes Belo Ramos Horta candidatura candidatos favoritos comité direitos humanos timor indonésia oslo",
-}
-
 # Ler XML de tópicos
 tree = ET.parse("folha/topicos.xml")
 root = tree.getroot()
@@ -29,7 +21,6 @@ results = []
 for top in topics:
     qid = top.find("num").text.strip()
     title = top.find("title").text.strip()
-    title = expansao_consulta.get(qid, title)
 
     query = qp.parse_query(title)
 
